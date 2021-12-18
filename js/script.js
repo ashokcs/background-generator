b528111324c50beecaf2eebd995183d6e720dd3e
DESKTOP-0V9VAH6\ashok <ashok.chakravarthi@zohocorp.com>
2021-12-18 19:22:23 +0530

    Random BG generator logic implemented using HTML, CSS, and JS.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 // If no argument is passed, 256 is set, ie, returns a number between and including 0 to 255
 // 		For example: 
 //    getRandomColorChannel(5) will return either 0, 1, 2, 3, 4
+let red = 0;
+let green = 0;
+let blue = 0;
 const getRandomColorChannel = function(options=256) {
   return Math.floor( Math.random() * options )
 }
@@ -14,9 +17,23 @@ console.log( getRandomColorChannel() )
 // COMPLETE THE FOLLOWING STEPS
 
 // 1) Write an rgb-color string using `getRandomColorChannel()` that can be set as the value for `background-color` in CSS
+function setNewColour()
+{
+  red = getRandomColorChannel();
+  green = getRandomColorChannel();
+  blue = getRandomColorChannel();
+  renderChanges();
+}
 
 // 2) Update user interface (the `document`), setting the new colour string to the body's background-color property
+const renderChanges = function() {
+  document.body.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  document.querySelector("#rgb").innerHTML = "<h2>RGB(" + red +", "+ green + ", "+ blue + ")</h2>";
+}
 
 // 3) Use the three individual RGB numbers to update the HTML element "rgb(###, ####, ###)" (replacing ###s with the values)
 
 // 4) Wrap the entire procedure in a function named `setNewColour()`, call it multiple times from the `console` to test
+const backgroundButton = document.querySelector(`#background`)
+backgroundButton.addEventListener(`click`, setNewColour)
+window.addEventListener(`load`, setNewColour)
